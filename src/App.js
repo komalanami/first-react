0c5977ab807264943dccab630a117b2b12ad54ac
komalanami <kumarikomal1511@gmail.com>
2025-07-03 21:58:44 +0530

    update for dynamic O/P control

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import react from './assets/react.png';
+
+const reactdescriptors = ['Fundamental', 'Cruical', 'Core'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const descriptors = reactdescriptors[getRandomInt(2)];
+
+  return (
+      <header>
+        <img src={react} className="React-logo" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {descriptors} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
     </div>
   );
 }
