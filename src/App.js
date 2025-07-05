7fd043898d5c115d9bb110a680f002b4f5f3a0b1
komalanami <kumarikomal1511@gmail.com>
2025-07-05 18:35:51 +0530

    fragement to remove unnecessary tag

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -70,7 +70,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
